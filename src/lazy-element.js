8f2fcd14fe6fc273603b2dbe9c20ce18e567c385
Chris Zempel <zempel@efeqdev.com>
2018-04-14 11:57:11 -0500

    Adds lots of configuration, adopts usage of prettier, working lit-element usage. Ready to roll

--- src/lazy-element.js
+++ src/lazy-element.js
@@ -10,11 +10,11 @@
 
 // Import statements in Polymer 3.0 can now use package names.
 // polymer-element.js now exports PolymerElement instead of Element,
-// so no need to change the symbol. 
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+// so no need to change the symbol.
+import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
 
 class LazyElement extends PolymerElement {
-  static get template () {
+  static get template() {
     // Template getter must return an instance of HTMLTemplateElement.
     // The html helper function makes this easy.
     return html`
@@ -24,4 +24,4 @@ class LazyElement extends PolymerElement {
 }
 
 // Register the element with the browser.
-customElements.define('lazy-element', LazyElement);
+customElements.define("lazy-element", LazyElement);
--- src/marsfarm-app.js
+++ src/marsfarm-app.js
@@ -10,24 +10,24 @@
 
 // Import statements in Polymer 3.0 can now use package names.
 // polymer-element.js now exports PolymerElement instead of Element,
-// so no need to change the symbol. 
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { LitElement, html } from '@polymer/lit-element/lit-element.js';
-import '@polymer/app-layout/app-header-layout/app-header-layout.js';
-import '@polymer/polymer/lib/elements/dom-if.js';
-import '@polymer/paper-checkbox/paper-checkbox.js';
+// so no need to change the symbol.
+import { PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { LitElement, html } from "@polymer/lit-element/lit-element.js";
+import "@polymer/app-layout/app-header-layout/app-header-layout.js";
+import "@polymer/polymer/lib/elements/dom-if.js";
+import "@polymer/paper-checkbox/paper-checkbox.js";
 
 class MarsfarmApp extends LitElement {
-  static get properties () {
+  static get properties() {
     return {
       message: {
         type: String,
-        value: ''
+        value: ""
       },
       pie: {
         type: Boolean,
         value: false,
-        observer: 'togglePie'
+        observer: "togglePie"
       },
       loadComplete: {
         type: Boolean,
@@ -37,45 +37,51 @@ class MarsfarmApp extends LitElement {
   }
 
   constructor() {
-    // If you override the constructor, always call the 
+    // If you override the constructor, always call the
     // superconstructor first.
     super();
-    this.message = 'Hello World! I\'m a Polymer element :)';
+    this.message = "Hello World! I'm a Polymer element :)";
+    this.onPieChanged = this.onPieChanged.bind(this);
   }
 
-  ready(){
-    // If you override ready, always call super.ready() first.
-    super.ready();
-    // Output the custom element's HTML tag to the browser console.
-    // Open your browser's developer tools to view the output.
-    console.log(this.tagName);
+  onPieChanged(e) {
+    this.pie = e.detail.value;
   }
-  
-  togglePie(){
-    if(this.pie && !this.loadComplete) {
+
+  togglePie() {
+    if (this.pie && !this.loadComplete) {
       // See https://developers.google.com/web/updates/2017/11/dynamic-import
-      import('./lazy-element.js').then((LazyElement) => {
-        console.log("LazyElement loaded");
-      }).catch((reason) => {
-        console.log("LazyElement failed to load", reason);
-      });
+      import("./lazy-element.js")
+        .then(LazyElement => {
+          console.log("LazyElement loaded");
+        })
+        .catch(reason => {
+          console.log("LazyElement failed to load", reason);
+        });
       this.loadComplete = true;
     }
   }
 
-  render() {
+  render({ pie, message }) {
     return html`
       <h1>Start Polymer 3.0</h1>
-      <p>[[message]]</p>
+      <p>${message}</p>
       <paper-checkbox 
-        checked={{pie}}>I like pie.</paper-checkbox>
-      <template is="dom-if" if=[[pie]]>
-        <lazy-element><p>lazy loading...</p></lazy-element>
-      </template>
+        checked="${pie}"
+        on-checked-changed=${this.onPieChanged}
+        >I like pie.</paper-checkbox>
+
+      ${
+        this.pie
+          ? html`
+            <lazy-element><p>lazy loading...</p></lazy-element>
+            <p>hello this is if</i>
+            `
+          : html`<p>not checked<p>`
+      }
     `;
   }
-
 }
 
 // Register the element with the browser.
-customElements.define('marsfarm-app', MarsfarmApp);
+customElements.define("marsfarm-app", MarsfarmApp);
